aea5839a5b67f3b6842a23170df801797ba9c1ab
johnmuthua <caveomuthama@gmail.com>
2024-01-17 20:19:47 +0300

    bug: homepage open bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import About from "@/components/about/About";
-import Footer from "@/components/footer/Footer";
-import Hero from "@/components/hero/Hero";
-import Services from "@/components/services/Services";
-import Subscribe from "@/components/subscribe/Subscribe";
-import Team from "@/components/team/Team";
-import Testimonials from "@/components/testimonial/Testimonials";
-import { Box } from "@mui/material";
+import About from '@/components/about/About';
+import Footer from '@/components/footer/Footer';
+import Hero from '@/components/hero/Hero';
+import Services from '@/components/services/Services';
+import Subscribe from '@/components/subscribe/Subscribe';
+import Team from '@/components/team/Team';
+import Testimonials from '@/components/testimonial/Testimonials';
+import { Box } from '@mui/material';
 
 export default function Home() {
   return (
-     <Box
+    <Box
       sx={{
         margin: '1rem',
       }}
@@ -22,5 +22,5 @@ export default function Home() {
       <Subscribe />
       <Footer />
     </Box>
-  )
+  );
 }
