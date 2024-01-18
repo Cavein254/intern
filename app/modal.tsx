66f8b87b8d5921691bf6a5bb15feab421ffb815b
johnmuthua <caveomuthama@gmail.com>
2024-01-18 10:53:22 +0300

    chore: packages upgrades

---
+++ app/modal.tsx
@@ -0,0 +1,26 @@
+import About from '@/components/about/About';
+import Footer from '@/components/footer/Footer';
+import Hero from '@/components/hero/Hero';
+import Services from '@/components/services/Services';
+import Subscribe from '@/components/subscribe/Subscribe';
+import Team from '@/components/team/Team';
+import Testimonials from '@/components/testimonial/Testimonials';
+import { Box } from '@mui/material';
+
+export default function Home() {
+  return (
+    <Box
+      sx={{
+        margin: '1rem',
+      }}
+    >
+      <Hero />
+      <About />
+      <Services />
+      <Testimonials />
+      <Team />
+      <Subscribe />
+      <Footer />
+    </Box>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,12 @@
-import About from '@/components/about/About';
-import Footer from '@/components/footer/Footer';
 import Hero from '@/components/hero/Hero';
-import Services from '@/components/services/Services';
-import Subscribe from '@/components/subscribe/Subscribe';
-import Team from '@/components/team/Team';
-import Testimonials from '@/components/testimonial/Testimonials';
 import { Box } from '@mui/material';
 
-export default function Home() {
+const page = () => {
   return (
-    <Box
-      sx={{
-        margin: '1rem',
-      }}
-    >
+    <Box>
       <Hero />
-      <About />
-      <Services />
-      <Testimonials />
-      <Team />
-      <Subscribe />
-      <Footer />
     </Box>
   );
-}
+};
+
+export default page;
