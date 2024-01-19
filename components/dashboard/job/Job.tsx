b8e3c1d6be8dfcaa76062ab7999fc278eb843fb0
johnmuthua <caveomuthama@gmail.com>
2024-01-19 16:46:09 +0300

    Add link to page

--- components/dashboard/job/Job.tsx
+++ components/dashboard/job/Job.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Typography } from '@mui/material';
+import Link from 'next/link';
 import { AiOutlineFieldNumber } from 'react-icons/ai';
 import { BiMoneyWithdraw, BiTime } from 'react-icons/bi';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs';
@@ -14,54 +15,57 @@ interface ItemProps {
   id: string | null;
 }
 const Job = ({ items }: { items: ItemProps }) => {
+  const link = `/api/job/${items}`;
   return (
-    <Box
-      sx={{
-        boxShadow: 2,
-        width: '100%',
-      }}
-      className="job-main-card"
-    >
-      <Box className="job-whole-wrapper">
-        <Box className="job-header-wrapper">
-          <Box className="job-card-items">
-            <Box>
-              <GiAbstract034 className="job-icon" />
+    <Link href={link}>
+      <Box
+        sx={{
+          boxShadow: 2,
+          width: '100%',
+        }}
+        className="job-main-card"
+      >
+        <Box className="job-whole-wrapper">
+          <Box className="job-header-wrapper">
+            <Box className="job-card-items">
+              <Box>
+                <GiAbstract034 className="job-icon" />
+              </Box>
+              <Box className="job-card-text">
+                <Typography variant="h4">{items.title}</Typography>
+                <Typography variant="h6">{items.locationType}</Typography>
+              </Box>
             </Box>
-            <Box className="job-card-text">
-              <Typography variant="h4">{items.title}</Typography>
-              <Typography variant="h6">{items.locationType}</Typography>
+            <Box className="job-apply-btn">
+              <Button variant="contained">
+                Apply <BsFillArrowRightCircleFill className="job-apply-icon" />
+              </Button>
             </Box>
           </Box>
-          <Box className="job-apply-btn">
-            <Button variant="contained">
-              Apply <BsFillArrowRightCircleFill className="job-apply-icon" />
-            </Button>
+          <Box className="job-details">
+            <Box className="job-item-detail">
+              <BiTime className="job-detail-item" />
+              <h6>{items.JobType}</h6>
+            </Box>
+            <Box className="job-item-detail">
+              <AiOutlineFieldNumber className="job-detail-item" />
+              <h6>3-8 years</h6>
+            </Box>
+            <Box className="job-item-detail">
+              <BiMoneyWithdraw className="job-detail-item" />
+              <h6>Ksh 120,000</h6>
+            </Box>
           </Box>
-        </Box>
-        <Box className="job-details">
-          <Box className="job-item-detail">
-            <BiTime className="job-detail-item" />
-            <h6>{items.JobType}</h6>
-          </Box>
-          <Box className="job-item-detail">
-            <AiOutlineFieldNumber className="job-detail-item" />
-            <h6>3-8 years</h6>
-          </Box>
-          <Box className="job-item-detail">
-            <BiMoneyWithdraw className="job-detail-item" />
-            <h6>Ksh 120,000</h6>
-          </Box>
-        </Box>
-        <Box>
           <Box>
-            <p>Flexible working hours</p>
-            <p>Great Medical Insurance</p>
-            <p>Amaizing working culture</p>
+            <Box>
+              <p>Flexible working hours</p>
+              <p>Great Medical Insurance</p>
+              <p>Amaizing working culture</p>
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+    </Link>
   );
 };
 
